73a94561dd17c40e22a9ed289bc6fb5fe3a40b52
Pasan47 <pasan.rathnayaka01.gmail.com>
2026-02-14 15:51:30 +0530

    studentJS

---
+++ auth.js
@@ -0,0 +1,7 @@
+const jwt = required("jsonwebtoken");
+
+module.exports = function authenticateToken(req, res, next) {
+    if (
+        req.headers.authorization &&
+    )
+}
---
+++ student.js
@@ -0,0 +1,44 @@
+const { useConnection } = require("./studentModel");
+
+Router.post("/login", async (req, res) => {
+    const username = req.body.userName;
+    const password = req.body.password;
+    const user = {
+        name: username,
+        password: password
+    };
+
+    const accessToken = jwt.sign(user, "ACCESS_TOKEN", {
+        expiresIn: "10s",
+    });
+
+    const refreshToken = jwt.sign(user, "Refresh_TOKEN", {
+        expiresIn: "24h",
+    });
+
+    res.cookie("accessToken", accessToken, { httpOnly: true });
+    res.cookie("refreshToken", refreshToken, { httpOnly: true });
+    res.send("accessToken", refreshToken);
+
+});
+
+Router.post("/token", (req, res) => {
+    const refreshToken = req.cookies.refreshToken;
+
+    if (refreshToken == null) res.sendStatus(401);
+
+    jwt.verify(refreshToken, process.env.RE_TOKEN_KEY, (err, user) => {
+        if (err) res.sendStatus(403);
+        const accessToken = jwt.sign({ name:user.name }, process.env.TOKEN_KEY,{
+            expiresIn: "10s",
+        });
+        res.send({ accessToken });
+    
+    });
+});
+
+Router.delete("/logout", (req, res) => {
+    res.clearCookie("refreshToken", { httpOnly: true });
+    res.clearCookie("accessToken", { httpOnly: true });
+});
+
