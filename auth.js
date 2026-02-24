12c2c3d7d11ebcadd6a735bf920ecaf79761bc36
Pasan47 <pasan.rathnayaka01.gmail.com>
2026-02-24 10:33:58 +0530

    edit

--- auth.js
+++ auth.js
@@ -1,7 +1,17 @@
-const jwt = required("jsonwebtoken");
+const jwt = require("jsonwebtoken");
+
 
 module.exports = function authenticateToken(req, res, next) {
     if (
         req.headers.authorization &&
-    )
-}
+        req.headers.authorization.startsWith("bearer")
+    ) {
+        const token = req.headers.authorization.split(" ")[1];
+        if (token == null) res.sendStatus(401);
+        jwt.verify(token, process.env.TOKEN_KEY, (err, user) => {
+            if (err) return res.sendStatus(403);
+            req.user = user;
+            next();
+        });
+    } else res.sendStatus(401);
+};
--- index.js
+++ index.js
@@ -18,7 +18,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const connectToDatabase = require("./connection");
-const Student = require("./studentModel");
+const login = require("./login");
+const student = require("./student");
+const studentModel = require("./studentModel");
 
 const app = express();
 const PORT = 5000;
@@ -27,33 +29,41 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser());
+
+
+app.use(express.json());
+app.use("/student", student);
+
 // MongoDB connection
 connectToDatabase();
 
 /* =========================
    POST API - Add Student
    ========================= */
-app.post("/api/students", async (req, res) => {
-  try {
-    const student = new Student(req.body);
-    await student.save();
-    res.status(201).json(student);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+// app.post("/api/students", async (req, res) => {
+//   try {
+//     const student = new Student(req.body);
+//     await student.save();
+//     res.status(201).json(student);
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// });
 
 /* =========================
    GET API - Get All Students
    ========================= */
-app.get("/api/students", async (req, res) => {
-  try {
-    const students = await Student.find();
-    res.json(students);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+// app.get("/api/students", async (req, res) => {
+//   try {
+//     const students = await Student.find();
+//     res.json(students);
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
 
 // start server
 app.listen(PORT, () => {
